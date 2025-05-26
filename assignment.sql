2bfc20893971dac04daebd4cbadbba3b272dc387
naziat <z.naziat@gmail.com>
2025-05-26 10:15:49 +0600

    second

--- assignment.sql
+++ assignment.sql
@@ -1,7 +1,50 @@
 -- Active: 1748140249333@@127.0.0.1@5432@conservation_db
-CREATE TABLE species (
-    id SERIAL PRIMARY KEY,
+--TABLE-1 rangers--
+CREATE TABLE rangers (
+    ranger_id SERIAL PRIMARY KEY,
     name VARCHAR(100),
-    status VARCHAR(50),
-    habitat VARCHAR(100)
+    region VARCHAR(100)
 );
+
+INSERT INTO rangers (name, region) VALUES
+('Alice Green', 'Northern Hills'),
+('Bob White', 'River Delta'),
+('Carol King', 'Mountain Range'); 
+
+SELECT * FROM rangers;
+
+--TABLE-2 species--
+CREATE TABLE species(
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(100),
+    scientific_name VARCHAR(150),
+    discovery_date DATE,
+    conservation_status VARCHAR(50)
+);
+INSERT INTO species (species_id, common_name, scientific_name, discovery_date, conservation_status) 
+VALUES
+(1, 'Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
+(2, 'Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
+(3, 'Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
+(4, 'Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
+
+
+SELECT * FROM species;
+
+--TABLE-3 species--
+CREATE TABLE sightings(
+    sighting_id SERIAL PRIMARY KEY,
+    ranger_id INTEGER REFERENCES "rangers"(ranger_id),
+    species_id INTEGER REFERENCES "species"(species_id),
+    sighting_time TIMESTAMP,
+    location VARCHAR(50)
+);
+
+INSERT INTO sightings (species_id, ranger_id, location, sighting_time) VALUES
+(1, 1, 'Peak Ridge', '2024-05-10 07:45:00'),
+(2, 2, 'Bankwood Area', '2024-05-12 16:20:00'),
+(3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00'),
+(1, 2, 'Snowfall Pass', '2024-05-18 18:30:00');
+
+SELECT * FROM sightings;
+DROP TABLE sightings;
